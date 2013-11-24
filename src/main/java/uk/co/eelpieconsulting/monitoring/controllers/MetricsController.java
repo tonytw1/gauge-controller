2d035b9d353acdfcc294728012e2f3f3abba733c
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-11-24 14:43:32 +0000

    Show all metrics screen.

---
+++ src/main/java/uk/co/eelpieconsulting/monitoring/controllers/MetricsController.java
@@ -0,0 +1,28 @@
+package uk.co.eelpieconsulting.monitoring.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import uk.co.eelpieconsulting.monitoring.MetricsDAO;
+
+@EnableAutoConfiguration
+@Controller
+public class MetricsController {
+
+	private final MetricsDAO metricsDAO;
+	
+	@Autowired
+	public MetricsController(MetricsDAO metricsDAO) {
+		this.metricsDAO = metricsDAO;
+	}
+	
+	@RequestMapping(value="/metrics", method=RequestMethod.GET)
+	public ModelAndView homepage() {
+		return new ModelAndView("metrics").addObject("availableMetrics", metricsDAO.getMetrics());
+	}	
+	
+}
