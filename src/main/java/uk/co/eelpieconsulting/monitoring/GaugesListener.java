c8da1798176f5e626624316bafe22c08826e5e58
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-03-05 21:30:20 +0000

    Register lamps.

--- src/main/java/uk/co/eelpieconsulting/monitoring/GaugesListener.java
+++ src/main/java/uk/co/eelpieconsulting/monitoring/GaugesListener.java
@@ -1,20 +1,22 @@
 package uk.co.eelpieconsulting.monitoring;
 
+import com.google.common.collect.Lists;
 import org.apache.log4j.Logger;
 import org.fusesource.mqtt.client.BlockingConnection;
 import org.fusesource.mqtt.client.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import uk.co.eelpieconsulting.monitoring.model.Gauge;
 import uk.co.eelpieconsulting.monitoring.model.GaugeType;
+
+import java.util.List;
 
 @Component
 public class GaugesListener {
 	
 	private final static Logger log = Logger.getLogger(GaugesListener.class);
 	
-	private static final String GAUGE_PREFIX = "gauge:";
+	private static final List<String> GAUGE_PREFIXES = Lists.newArrayList("gauge", "lamp");
 	
 	private final MqttConnectionFactory mqttConnectionFactory;
 	
@@ -43,9 +45,13 @@ public class GaugesListener {
 		        	String messageString = new String(payload, "UTF-8");
 		        	log.debug("Got gauges message: " + messageString);
 		        	try {
-		        		if (messageString.startsWith(GAUGE_PREFIX)) {
-		        			String gaugeDescripition = messageString.split(GAUGE_PREFIX)[1];
-		        			gaugeDAO.registerGauge(parseGaugeDescription(gaugeDescripition));
+
+						for (String gaugePrefix: GAUGE_PREFIXES) {
+							final String prefix = gaugePrefix + ":";
+							if (messageString.startsWith(prefix)) {
+								final String gaugeDescripition = messageString.split(prefix)[1];
+								gaugeDAO.registerGauge(parseGaugeDescription(gaugeDescripition));
+							}
 		        		}		        		
 		        	} catch (Exception e) {
 		        		log.error(e);
