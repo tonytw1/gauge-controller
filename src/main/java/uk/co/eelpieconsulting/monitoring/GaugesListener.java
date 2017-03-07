bc3215b13e5430b06962c67b2a1090d1ec386cb5
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-03-07 14:00:24 +0000

    Pick up full scalae deflections from gauge announcements.

--- src/main/java/uk/co/eelpieconsulting/monitoring/GaugesListener.java
+++ src/main/java/uk/co/eelpieconsulting/monitoring/GaugesListener.java
@@ -1,28 +1,24 @@
 package uk.co.eelpieconsulting.monitoring;
 
-import com.google.common.collect.Lists;
 import org.apache.log4j.Logger;
 import org.fusesource.mqtt.client.BlockingConnection;
 import org.fusesource.mqtt.client.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.co.eelpieconsulting.monitoring.model.Gauge;
-import uk.co.eelpieconsulting.monitoring.model.GaugeType;
-
-import java.util.List;
 
 @Component
 public class GaugesListener {
 	
 	private final static Logger log = Logger.getLogger(GaugesListener.class);
-	
-	private static final List<String> GAUGE_PREFIXES = Lists.newArrayList("gauge", "lamp");
-	
+
 	private final MqttConnectionFactory mqttConnectionFactory;
-	
+	private final GaugeAnnouncementParser gaugeAnnouncementParser;
+
 	@Autowired
-	public GaugesListener(GaugeDAO gaugeDAO, MqttConnectionFactory mqttConnectionFactory) throws Exception {	   
-		this.mqttConnectionFactory = mqttConnectionFactory;		
+	public GaugesListener(GaugeDAO gaugeDAO, MqttConnectionFactory mqttConnectionFactory, GaugeAnnouncementParser gaugeAnnouncementParser) throws Exception {
+		this.mqttConnectionFactory = mqttConnectionFactory;
+		this.gaugeAnnouncementParser = gaugeAnnouncementParser;
 		new Thread(new Listener(gaugeDAO)).start();
 	}
 
@@ -44,27 +40,16 @@ public class GaugesListener {
 		        	byte[] payload = message.getPayload();
 		        	String messageString = new String(payload, "UTF-8");
 		        	log.info("Got gauges message: " + messageString);
-		        	try {
-						for (String gaugePrefix: GAUGE_PREFIXES) {
-							final String prefix = gaugePrefix + ":";
-							if (messageString.startsWith(prefix)) {
-								log.info("Processing guage message for prefix (" + prefix + ") :" + messageString);
-								final String gaugeDescription = messageString.split(":")[1];
-								gaugeDAO.registerGauge(parseGaugeDescription(gaugePrefix, gaugeDescription));
-							}
-		        		}		        		
-		        	} catch (Exception e) {
-		        		log.error(e);
-		        	}
+					Gauge possibleGauge = gaugeAnnouncementParser.parse(messageString);
+					if (possibleGauge != null) {
+						gaugeDAO.registerGauge(possibleGauge);
+					}
 		        }
 		        
 			} catch (Exception e) {
 				log.error(e);
 			}
 		}
-
-		private Gauge parseGaugeDescription(String gaugePrefix, String gaugeDescripition) {
-			return new Gauge(gaugeDescripition, GaugeType.valueOf(gaugePrefix.toUpperCase()));
-		}
 	}
+
 }
--- src/main/java/uk/co/eelpieconsulting/monitoring/model/Gauge.java
+++ src/main/java/uk/co/eelpieconsulting/monitoring/model/Gauge.java
@@ -4,10 +4,12 @@ public class Gauge  {
 
 	private final String name;
 	private final GaugeType type;
+	private final int fsd;
 
-	public Gauge(String name, GaugeType type) {
+	public Gauge(String name, GaugeType type, int fsd) {
 		this.name = name;
 		this.type = type;
+		this.fsd = fsd;
 	}
 	
 	public String getName() {
@@ -17,35 +19,39 @@ public class Gauge  {
 	public GaugeType getType() {
 		return type;
 	}
-	
+
+	public int getFsd() {
+		return fsd;
+	}
+
 	@Override
-	public String toString() {
-		return "Gauge [name=" + name + ", type=" + type + "]";
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Gauge gauge = (Gauge) o;
+
+		if (fsd != gauge.fsd) return false;
+		if (name != null ? !name.equals(gauge.name) : gauge.name != null) return false;
+		return type == gauge.type;
+
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + (type != null ? type.hashCode() : 0);
+		result = 31 * result + fsd;
 		return result;
 	}
-	
+
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Gauge other = (Gauge) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
+	public String toString() {
+		return "Gauge{" +
+				"name='" + name + '\'' +
+				", type=" + type +
+				", fsd=" + fsd +
+				'}';
 	}
-	
+
 }
