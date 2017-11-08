5fe0bc39329bf14afbd6d290821173fd0da0e15f
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-11-08 22:48:12 +0000

    Trim line break from messages.

--- src/main/java/uk/co/eelpieconsulting/monitoring/GaugesListener.java
+++ src/main/java/uk/co/eelpieconsulting/monitoring/GaugesListener.java
@@ -9,47 +9,48 @@ import uk.co.eelpieconsulting.monitoring.model.Gauge;
 
 @Component
 public class GaugesListener {
-	
-	private final static Logger log = Logger.getLogger(GaugesListener.class);
 
-	private final MqttConnectionFactory mqttConnectionFactory;
-	private final GaugeAnnouncementParser gaugeAnnouncementParser;
+  private final static Logger log = Logger.getLogger(GaugesListener.class);
 
-	@Autowired
-	public GaugesListener(GaugeDAO gaugeDAO, MqttConnectionFactory mqttConnectionFactory, GaugeAnnouncementParser gaugeAnnouncementParser) throws Exception {
-		this.mqttConnectionFactory = mqttConnectionFactory;
-		this.gaugeAnnouncementParser = gaugeAnnouncementParser;
-		new Thread(new Listener(gaugeDAO)).start();
-	}
+  private final MqttConnectionFactory mqttConnectionFactory;
+  private final GaugeAnnouncementParser gaugeAnnouncementParser;
 
-	private class Listener implements Runnable {
-		
-		private final GaugeDAO gaugeDAO;
+  @Autowired
+  public GaugesListener(GaugeDAO gaugeDAO, MqttConnectionFactory mqttConnectionFactory, GaugeAnnouncementParser gaugeAnnouncementParser) throws Exception {
+    this.mqttConnectionFactory = mqttConnectionFactory;
+    this.gaugeAnnouncementParser = gaugeAnnouncementParser;
+    new Thread(new Listener(gaugeDAO)).start();
+  }
 
-		public Listener(GaugeDAO gaugeDAO) {
-			this.gaugeDAO = gaugeDAO;
-		}
+  private class Listener implements Runnable {
 
-		@Override
-		public void run() {
-			try {
-				BlockingConnection connection = mqttConnectionFactory.subscribeToGaugesTopic();
-		        
-		        while (true) {        	
-		        	final Message message = connection.receive();
-		        	byte[] payload = message.getPayload();
-		        	String messageString = new String(payload, "UTF-8");
-		        	log.info("Got gauges message: " + messageString);
-					Gauge possibleGauge = gaugeAnnouncementParser.parse(messageString);
-					if (possibleGauge != null) {
-						gaugeDAO.registerGauge(possibleGauge);
-					}
-		        }
-		        
-			} catch (Exception e) {
-				log.error(e);
-			}
-		}
-	}
+    private final GaugeDAO gaugeDAO;
+
+    public Listener(GaugeDAO gaugeDAO) {
+      this.gaugeDAO = gaugeDAO;
+    }
+
+    @Override
+    public void run() {
+      try {
+        BlockingConnection connection = mqttConnectionFactory.subscribeToGaugesTopic();
+
+        while (true) {
+          final Message message = connection.receive();
+          byte[] payload = message.getPayload();
+          String messageString = new String(payload, "UTF-8").trim();
+          log.debug("Got gauges message: '" + messageString + "'");
+          Gauge parsedGauge = gaugeAnnouncementParser.parse(messageString);
+          if (parsedGauge != null) {
+            log.debug("Registering gauge: " + parsedGauge);
+            gaugeDAO.registerGauge(parsedGauge);
+          }
+        }
+
+      } catch (Exception e) {
+        log.error(e);
+      }
+    }
+  }
 
 }
