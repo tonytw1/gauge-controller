3791c3a3a42d7446963cb9bfea0023a8b5a68a44
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-06-26 10:28:20 +0100

    Export metrics in (vagely) prometheus format.

--- src/main/java/uk/co/eelpieconsulting/monitoring/controllers/MetricsController.java
+++ src/main/java/uk/co/eelpieconsulting/monitoring/controllers/MetricsController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import uk.co.eelpieconsulting.monitoring.MetricsDAO;
+import uk.co.eelpieconsulting.monitoring.model.Metric;
+
+import java.util.List;
 
 @Controller
 public class MetricsController {
@@ -19,9 +22,15 @@ public class MetricsController {
 		this.metricsDAO = metricsDAO;
 	}
 	
-	@RequestMapping(value="/metrics", method=RequestMethod.GET)
-	public ModelAndView homepage() {
+	@RequestMapping(value="/browse", method=RequestMethod.GET)
+	public ModelAndView browse() {
 		return new ModelAndView("templates/metrics").addObject("availableMetrics", metricsDAO.getMetrics());
-	}	
-	
+	}
+
+	@RequestMapping(value="/metrics", method=RequestMethod.GET)
+	public ModelAndView metrics() {
+		List<Metric> metrics = metricsDAO.getMetrics();
+		return new ModelAndView("templates/export").addObject("availableMetrics", metrics);
+	}
+
 }
