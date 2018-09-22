91c3d36324169896479657481ecd47e8772817a6
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-22 22:20:23 +0100

    Rounding of scaled values.

--- src/main/java/uk/co/eelpieconsulting/monitoring/model/transforms/Scaled.java
+++ src/main/java/uk/co/eelpieconsulting/monitoring/model/transforms/Scaled.java
@@ -2,22 +2,27 @@ package uk.co.eelpieconsulting.monitoring.model.transforms;
 
 import uk.co.eelpieconsulting.monitoring.model.Metric;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Scaled implements Transform {
 
-  private double scale;
+    private BigDecimal scale;
 
-  public Scaled(double scale) {
-    this.scale = scale;
-  }
+    public Scaled(double scale) {
+        this.scale = BigDecimal.valueOf(scale);
+    }
 
-  @Override
-  public String getName() {
-    return "x " + scale;
-  }
+    @Override
+    public String getName() {
+        return "x " + scale;
+    }
 
-  @Override
-  public String transform(Metric metric) {
-    return Double.toString(Double.parseDouble(metric.getLastValue()) * scale);
-  }
+    @Override
+    public String transform(Metric metric) {
+        BigDecimal metricValue = BigDecimal.valueOf(Double.parseDouble(metric.getLastValue()));
+        BigDecimal scaled = metricValue.multiply(scale).setScale(0, RoundingMode.HALF_UP);
+        return scaled.toPlainString();
+    }
 
 }
