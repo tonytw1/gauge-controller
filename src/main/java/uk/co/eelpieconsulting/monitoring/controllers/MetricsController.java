cdea9b08c37da9b67a7f7f4d0bddbc1b84349c27
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-24 17:39:17 +0100

    Additional end point for transformed metrics only.

--- src/main/java/uk/co/eelpieconsulting/monitoring/controllers/MetricsController.java
+++ src/main/java/uk/co/eelpieconsulting/monitoring/controllers/MetricsController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import uk.co.eelpieconsulting.monitoring.MetricsDAO;
+import uk.co.eelpieconsulting.monitoring.RoutingDAO;
+import uk.co.eelpieconsulting.monitoring.TransformsDAO;
 import uk.co.eelpieconsulting.monitoring.model.Metric;
+import uk.co.eelpieconsulting.monitoring.model.MetricRouting;
+import uk.co.eelpieconsulting.monitoring.model.transforms.Transform;
 
 import java.util.List;
 import java.util.regex.Pattern;
@@ -18,33 +22,60 @@ public class MetricsController {
   private static final Pattern NO_ODD_CHARACTERS = Pattern.compile("[^a-zA-Z0-9]+");
 
   private final MetricsDAO metricsDAO;
+  private final RoutingDAO routingDAO;
+  private final TransformsDAO transformsDAO;
 
   @Autowired
-	public MetricsController(MetricsDAO metricsDAO) {
-		this.metricsDAO = metricsDAO;
-	}
-	
-	@RequestMapping(value="/browse", method=RequestMethod.GET)
-	public ModelAndView browse() {
-		return new ModelAndView("templates/browse").addObject("availableMetrics", metricsDAO.getMetrics());
-	}
+  public MetricsController(MetricsDAO metricsDAO, RoutingDAO routingDAO, TransformsDAO transformsDAO) {
+    this.metricsDAO = metricsDAO;
+    this.routingDAO = routingDAO;
+    this.transformsDAO = transformsDAO;
+  }
 
-	@RequestMapping(value="/metrics", method=RequestMethod.GET)
-	public ModelAndView metrics() {
-		List<Metric> metrics = metricsDAO.getMetrics();
+  @RequestMapping(value = "/browse", method = RequestMethod.GET)
+  public ModelAndView browse() {
+    return new ModelAndView("templates/browse").addObject("availableMetrics", metricsDAO.getMetrics());
+  }
+
+  @RequestMapping(value = "/metrics", method = RequestMethod.GET)
+  public ModelAndView metrics() {
+    List<Metric> metrics = metricsDAO.getMetrics();
 
     List<Metric> cleaned = Lists.newArrayList();
-    for (Metric m: metrics) {
-		  String safeName = NO_ODD_CHARACTERS.matcher(m.getName()).replaceAll("");
-
-		  try {
-		    Double.parseDouble(m.getLastValue());
-        cleaned.add(new Metric(safeName, m.getType(), m.getLastValue(), m.getDate()));
+    for (Metric m : metrics) {
+      try {
+        Double.parseDouble(m.getLastValue());
+        cleaned.add(new Metric(makeSafeName(m), m.getType(), m.getLastValue(), m.getDate()));
       } catch (Exception e) {
+        // TODO
       }
     }
 
-		return new ModelAndView("templates/export").addObject("availableMetrics", cleaned);
-	}
+    return new ModelAndView("templates/export").addObject("availableMetrics", cleaned);
+  }
+
+  @RequestMapping(value = "/transformed", method = RequestMethod.GET)
+  public ModelAndView routed() {
+
+    List<Metric> cleaned = Lists.newArrayList();
+
+    for (MetricRouting route : routingDAO.getGaugeRoutes().values()) {
+      if (route.getTransform() != null) {
+        Metric metric = metricsDAO.getByName(route.getMetricName());
+        if (metric != null) {
+          Transform transform = transformsDAO.transformByName(route.getTransform().getName());
+          String value = transform.transform(metric);
+          Double.parseDouble(value);
+          cleaned.add(new Metric(makeSafeName(metric), metric.getType(), value, metric.getDate()));
+        }
+      }
+    }
+
+    return new ModelAndView("templates/export").addObject("availableMetrics", cleaned);
+  }
+
+  private String makeSafeName(Metric m) {
+    return NO_ODD_CHARACTERS.matcher(m.getName()).replaceAll("");
+  }
 
 }
