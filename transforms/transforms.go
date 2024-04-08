a1599894943a9a53d5420aeaf877a4e03df37da7
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-04-08 10:47:15 +0100

    Inverted boolean transform.

--- transforms/transforms.go
+++ transforms/transforms.go
@@ -19,9 +19,22 @@ var booleanToInt = func(input string) (int, error) {
 	}
 }
 
+var invertedBooleanToInt = func(input string) (int, error) {
+	i, err := booleanToInt(input)
+	if err != nil {
+		return 0, err
+	}
+	if i == 1 {
+		return 0, nil
+	} else {
+		return 1, nil
+	}
+}
+
 var transforms = map[string]model.Transform{
-	"to_int":         {Name: "to_int", Transform: toInt},
-	"boolean_to_int": {Name: "boolean_to_int", Transform: booleanToInt},
+	"to_int":                  {Name: "to_int", Transform: toInt},
+	"boolean_to_int":          {Name: "boolean_to_int", Transform: booleanToInt},
+	"inverted_boolean_to_int": {Name: "inverted_boolean_to_int", Transform: invertedBooleanToInt},
 }
 
 func Transforms() map[string]model.Transform {
