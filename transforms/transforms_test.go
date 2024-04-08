242011cd659e6f1ddebf1ca0c5c299c737e5df76
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-04-08 10:17:31 +0100

    Asserts

--- transforms/transforms_test.go
+++ transforms/transforms_test.go
@@ -2,8 +2,13 @@ package transforms
 
 import "testing"
 
-func TestGetTransformByName(t *testing.T) {
+func TestGetTransformByName_CanFindTransformByNameAndCallIt(t *testing.T) {
 	transform, ok := GetTransformByName("to_int")
-	println(ok)
-	println(transform("123"))
+	if !ok {
+		t.Fatalf("Expected to find transform by name")
+	}
+	transformed, _ := transform("123")
+	if !(transformed == 123) {
+		t.Fatalf("Expected to transform '123' to 123")
+	}
 }
