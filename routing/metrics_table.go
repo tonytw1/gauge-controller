b65f89d80cd7fbf47044d6d874b4420af5e4b298
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-08-22 10:19:33 +0100

    Pointers.

--- routing/metrics_table.go
+++ routing/metrics_table.go
@@ -9,11 +9,11 @@ type MetricsTable struct {
 	metrics sync.Map
 }
 
-func (svc MetricsTable) AddMetrics(metric model.Metric) {
+func (svc *MetricsTable) AddMetrics(metric model.Metric) {
 	svc.metrics.Store(metric.Name, metric)
 }
 
-func (svc MetricsTable) AllMetrics() []model.Metric {
+func (svc *MetricsTable) AllMetrics() []model.Metric {
 	var ms = make([]model.Metric, 0)
 	svc.metrics.Range(func(k, v interface{}) bool {
 		ms = append(ms, v.(model.Metric))
@@ -22,7 +22,7 @@ func (svc MetricsTable) AllMetrics() []model.Metric {
 	return ms
 }
 
-func (svc MetricsTable) GetMetric(metricName string) (any, bool) {
+func (svc *MetricsTable) GetMetric(metricName string) (any, bool) {
 	return svc.metrics.Load(metricName) // TODO push any up
 }
 
